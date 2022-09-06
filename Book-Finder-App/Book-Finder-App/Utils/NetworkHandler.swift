c3efb8e87e43bcd3ff819e1a1a84af9322470b57
doogie97 <chris_97@naver.com>
2022-09-06 20:08:00 +0900

    💎feat : NetworkHandler 생성

---
+++ Book-Finder-App/Book-Finder-App/Utils/NetworkHandler.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkHandler.swift
+//  Book-Finder-App
+//
+//  Created by 최최성균 on 2022/09/06.
+//
+
+import Foundation
+
+struct NetworkHandler {
+    private let session = URLSession.shared
+    private let baseURL = "https://www.googleapis.com/books/v1/volumes/"
+    
+}
