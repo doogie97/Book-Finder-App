10435860369d46c78ff0e2dffb9f8e747c49213f
doogie97 <chris_97@naver.com>
2022-09-06 19:09:20 +0900

    💎feat : APIError 구현

---
+++ Book-Finder-App/Book-Finder-App/Utils/Error/APIError.swift
@@ -0,0 +1,14 @@
+//
+//  APIError.swift
+//  Book-Finder-App
+//
+//  Created by 최최성균 on 2022/09/06.
+//
+
+enum APIError: Error {
+    case bookTitleError
+    case urlError
+    case transportError
+    case responseError
+    case dataError
+}
