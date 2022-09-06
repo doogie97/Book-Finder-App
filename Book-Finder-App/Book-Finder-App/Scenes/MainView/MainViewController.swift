392e9db23afc37b776db03a5c17652fef0277f20
doogie97 <chris_97@naver.com>
2022-09-07 04:00:03 +0900

    💎feat : bookListCollectionView와 items연결

--- Book-Finder-App/Book-Finder-App/Scenes/MainView/MainViewController.swift
+++ Book-Finder-App/Book-Finder-App/Scenes/MainView/MainViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import RxSwift
+import RxCocoa
 
 final class MainViewController: UIViewController {
     private let viewModel: MainViewModelable
@@ -25,6 +26,18 @@ final class MainViewController: UIViewController {
     
     override func loadView() {
         self.view = mainView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindView()
+    }
+    
+    private func bindView() {
+        viewModel.items.bind(to: mainView.bookListCollectionView.rx.items(cellIdentifier: "\(BookListCell.self)", cellType: BookListCell.self)) { index, bookInfo, cell in
+            
+        }
+        .disposed(by: disposeBag)
     }
 }
 
