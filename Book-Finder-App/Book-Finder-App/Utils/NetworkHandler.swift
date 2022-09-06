a8d7da5fe2a41af95ad3872967406100213a5d97
doogie97 <chris_97@naver.com>
2022-09-06 23:19:46 +0900

    💎feat : 매개변수로 APIable을 받아 URL을 반환하는 메서드 생성

--- Book-Finder-App/Book-Finder-App/Utils/NetworkHandler.swift
+++ Book-Finder-App/Book-Finder-App/Utils/NetworkHandler.swift
@@ -11,4 +11,14 @@ struct NetworkHandler {
     private let session = URLSession.shared
     private let baseURL = "https://www.googleapis.com/books/v1/volumes/"
     
+    
+    private func makeURL(api: APIable) -> URL? {
+        var component = URLComponents(string: baseURL)
+        
+        component?.queryItems = api.params?.compactMap {
+            URLQueryItem(name: $0.key, value: $0.value)
+        }
+        
+        return component?.url
+    }
 }
