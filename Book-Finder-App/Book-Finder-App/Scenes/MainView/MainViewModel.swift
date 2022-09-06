18a159329d63868d50ee5840f17a4fbb310afe09
doogie97 <chris_97@naver.com>
2022-09-07 03:58:19 +0900

    💎feat : MainViewModel output 정의(totalItems, items)

--- Book-Finder-App/Book-Finder-App/Scenes/MainView/MainViewModel.swift
+++ Book-Finder-App/Book-Finder-App/Scenes/MainView/MainViewModel.swift
@@ -5,11 +5,16 @@
 //  Created by 최최성균 on 2022/09/07.
 //
 
+import RxRelay
+
 protocol MainViewModelable: MainViewModelInput, MainViewModelOutput {}
 
 protocol MainViewModelInput {}
 
-protocol MainViewModelOutput {}
+protocol MainViewModelOutput {
+    var totalItems: BehaviorRelay<Int> { get }
+    var items: BehaviorRelay<[Int]> { get }
+}
 
 final class MainViewModel: MainViewModelable {
     private let networkHandler: NetworkHandler
@@ -19,4 +24,8 @@ final class MainViewModel: MainViewModelable {
         self.networkHandler = networkHandler
         self.dataDecoder = dataDecoder
     }
+    
+    //out
+    let totalItems = BehaviorRelay<Int>(value: 0)
+    let items = BehaviorRelay<[Int]>(value: [1, 2, 3, 4, 5]) //콜렉션 뷰 확인을 위한 임시 코드
 }
