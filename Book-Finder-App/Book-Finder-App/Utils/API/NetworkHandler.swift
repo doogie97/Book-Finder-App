895733bbdb4ab1a263d79a6a8d3eba2b65111574
doogie97 <chris_97@naver.com>
2022-09-08 00:46:07 +0900

    💎feat : 옵저버블을 반환하는 networkhandler 구현 #8

--- Book-Finder-App/Book-Finder-App/Utils/API/NetworkHandler.swift
+++ Book-Finder-App/Book-Finder-App/Utils/API/NetworkHandler.swift
@@ -5,6 +5,7 @@
 //  Created by 최최성균 on 2022/09/06.
 //
 
+import RxSwift
 import Foundation
 
 struct NetworkHandler {
@@ -41,6 +42,56 @@ struct NetworkHandler {
             }
         }
         dataTask.resume()
+    }
+    
+    private func makeURL(api: APIable) -> URL? {
+        var component = URLComponents(string: baseURL)
+        
+        component?.queryItems = api.params?.compactMap {
+            URLQueryItem(name: $0.key, value: $0.value)
+        }
+        
+        return component?.url
+    }
+}
+
+struct NetworkHandler2 {
+    private let session = URLSession.shared
+    private let baseURL = "https://www.googleapis.com/books/v1/volumes/"
+    
+    func request(api: APIable) throws -> Observable<Data> {
+        guard let url = makeURL(api: api) else {
+            throw APIError.urlError
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = api.method.string
+        
+        return Observable<Data>.create() { emitter in
+            let dataTask = session.dataTask(with: request) { data, response, error in
+                DispatchQueue.main.async {
+                    guard error == nil else {
+                        emitter.onError(APIError.transportError)
+                        return
+                    }
+                    
+                    guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
+                        emitter.onError(APIError.responseError)
+                        return
+                    }
+
+                    guard let data = data else {
+                        emitter.onError(APIError.dataError)
+                        return
+                    }
+                    
+                    emitter.onNext(data)
+                }
+            }
+            dataTask.resume()
+            
+            return Disposables.create()
+        }
     }
     
     private func makeURL(api: APIable) -> URL? {
