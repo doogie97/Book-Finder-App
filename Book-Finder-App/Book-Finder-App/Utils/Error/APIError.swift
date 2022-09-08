eb15bcc74f8da8a298eaf87a3b8b32e8b2fe5d65
doogie97 <chris_97@naver.com>
2022-09-08 12:31:11 +0900

    ♻️refactor : APIError의 errorDescription 정의

--- Book-Finder-App/Book-Finder-App/Utils/Error/APIError.swift
+++ Book-Finder-App/Book-Finder-App/Utils/Error/APIError.swift
@@ -11,4 +11,8 @@ enum APIError: Error {
     case responseError
     case dataError
     case decodeError
+    
+    var errorDescription: String {
+        return "서버 통신 중 오류가 발생했습니다(Error code: \(self))"
+    }
 }
