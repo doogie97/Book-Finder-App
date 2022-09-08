f4998baaf8a5f11dcf3af0eacd184c6bdeb8ac2d
doogie97 <chris_97@naver.com>
2022-09-08 16:32:59 +0900

    💎feat : BookDetailView에 backButton 추가

--- Book-Finder-App/Book-Finder-App/Scenes/BookDetailView/BookDetailView.swift
+++ Book-Finder-App/Book-Finder-App/Scenes/BookDetailView/BookDetailView.swift
@@ -5,8 +5,43 @@
 //  Created by 최최성균 on 2022/09/08.
 //
 
-import UIKit
+import SnapKit
 
 final class BookDetailView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setLayout()
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private lazy var navigationView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        return view
+    }()
+    
+    private(set) lazy var backButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+        
+        return button
+    }()
+    
+    private func setLayout() {
+        self.backgroundColor = .systemBackground
+        
+        self.addSubview(navigationView)
+        navigationView.addSubview(backButton)
+        
+        navigationView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+        }
+        
+        backButton.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(self.safeAreaInset.top)
+        }
+    }
 }
