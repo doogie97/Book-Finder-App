333d40b893ef3e3c1a95ff5f387ad8b4a1ed2dc0
doogie97 <chris_97@naver.com>
2022-09-08 15:37:26 +0900

    💎feat : BookDetailViewController에 viewModel 생성

--- Book-Finder-App/Book-Finder-App/Scenes/BookDetailView/BookDetailViewController.swift
+++ Book-Finder-App/Book-Finder-App/Scenes/BookDetailView/BookDetailViewController.swift
@@ -8,5 +8,15 @@
 import UIKit
 
 final class BookDetailViewController: UIViewController {
+    private let viewModel: BookDetailViewModelable
+    
+    init(viewModel: BookDetailViewModelable) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
 }
