798f8b06847e6ab2edd2ad4ca9e4bd3957b34bdf
doogie97 <chris_97@naver.com>
2022-09-09 16:44:19 +0900

    🔬test : NetworkManager의 테스트 케이스 수정 및 추가

--- Book-Finder-App/Book-Finder-AppTests/Book_Finder_AppTests.swift
+++ Book-Finder-App/Book-Finder-AppTests/Book_Finder_AppTests.swift
@@ -55,30 +55,113 @@ class Book_Finder_AppTests: XCTestCase {
     }
     
     //MARK: - NetworkManager 테스트
-    func test_행복_이라는title로_NetworkManager의_request메서드호출시_첫번째책의title이_행복을_풀다_와일치하는지() {
-        //api상황에 따라 결과 상이할 수 있어 실제 api와 비교 테스트 필요
-        
+    // 테스트를 위한 메서드
+    func makeDummyData(data: Data?, statusCode: Int, error: Error?) -> ResponseResult {
+        let rsponse = HTTPURLResponse(url: URL(string: "test")!, statusCode: statusCode, httpVersion: "2", headerFields: nil)
+        return ResponseResult(data: data, response: rsponse, error: error)
+    }
+    
+    func test_문자열_행복_을Data로변환해_netWorkHandler의_request호출시_정상적으로가져오는지() {
         //given
-        let promise = expectation(description: "행복을 풀다와 일치하는지")
-
-        let networkManager = NetworkManger()
-        let dataDecoder = DataDecoder()
-        
-        networkManager.request(api: APIModel(bookTitle: "행복", startIndex: 0, maxResult: 10, method: .get)) { apiResult in
-            switch apiResult {
+        let promise = expectation(description: "convertError와 일치하는지")
+        let testString = "행복"
+        let dummyData = makeDummyData(data: testString.data(using: .utf8), statusCode: 200, error: nil)
+        let netWorkHandler = NetworkManger(urlSession: StubURLSession(dummyData: dummyData))
+        let testAPIModel = TestAPIModel(bookTitle: "", host: "", path: "", method: .get)
+        //when
+        netWorkHandler.request(api: testAPIModel) { data in
+            switch data {
             case .success(let data):
-                guard let searchResult = try? dataDecoder.parse(data: data, resultType: SearchResult.self) else {
-                    XCTFail()
-                    return
-                }
-                let result = searchResult.items?[0].volumeInfo?.title
-                print(searchResult)
-                XCTAssertEqual(result, "행복을 풀다")
+                let string = String(data: data, encoding: .utf8)
+                //then
+                XCTAssertEqual(string, testString)
             case .failure(_):
                 XCTFail()
             }
             promise.fulfill()
         }
-        wait(for: [promise], timeout: 10)
+        wait(for: [promise], timeout: 5)
+    }
+    
+    
+    func test_netWorkManger의_request호출시_url이_올바르지않을경우_convertError를_잘던지는지() {
+        //given
+        let promise = expectation(description: "convertError와 일치하는지")
+        let dummyData = makeDummyData(data: Data(), statusCode: 200, error: nil)
+        let netWorkHandler = NetworkManger(urlSession: StubURLSession(dummyData: dummyData))
+        let worongString = "   "
+        let testAPIModel = TestAPIModel(bookTitle: "", host: worongString, path: "", method: .get)
+        //when
+        netWorkHandler.request(api: testAPIModel) { data in
+            switch data {
+            case .success(_):
+                XCTFail()
+            case .failure(let error):
+                //then
+                XCTAssertEqual(error, APIError.urlError)
+            }
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 5)
+    }
+    
+    func test_netWorkManger의_request호출시_통신과정에error가있을경우_transportError를_잘던지는지() {
+        //given
+        let promise = expectation(description: "transportError와 일치하는지")
+        let dummyData = makeDummyData(data: Data(), statusCode: 200, error: APIError.decodeError)
+        let netWorkHandler = NetworkManger(urlSession: StubURLSession(dummyData: dummyData))
+        let testAPIModel = TestAPIModel(bookTitle: "", host: "", path: "", method: .get)
+        //when
+        netWorkHandler.request(api: testAPIModel) { data in
+            switch data {
+            case .success(_):
+                XCTFail()
+            case .failure(let error):
+                //then
+                XCTAssertEqual(error, APIError.transportError)
+            }
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 5)
+    }
+    
+    func test_netWorkManger의_request호출시_statusCode가_불안정할경우_responseError를_잘던지는지() {
+        //given
+        let promise = expectation(description: "responseError와 일치하는지")
+        let dummyData = makeDummyData(data: Data(), statusCode: 404, error: nil)
+        let netWorkHandler = NetworkManger(urlSession: StubURLSession(dummyData: dummyData))
+        let testAPIModel = TestAPIModel(bookTitle: "", host: "", path: "", method: .get)
+        //when
+        netWorkHandler.request(api: testAPIModel) { data in
+            switch data {
+            case .success(_):
+                XCTFail()
+            case .failure(let error):
+                //then
+                XCTAssertEqual(error, APIError.responseError)
+            }
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 5)
+    }
+    
+    func test_netWorkManger의_request호출시_data가nil일경우_dataError를_잘던지는지() {
+        //given
+        let promise = expectation(description: "dataError와 일치하는지")
+        let dummyData = makeDummyData(data: nil, statusCode: 200, error: nil)
+        let netWorkHandler = NetworkManger(urlSession: StubURLSession(dummyData: dummyData))
+        let testAPIModel = TestAPIModel(bookTitle: "", host: "", path: "", method: .get)
+        //when
+        netWorkHandler.request(api: testAPIModel) { data in
+            switch data {
+            case .success(_):
+                XCTFail()
+            case .failure(let error):
+                //then
+                XCTAssertEqual(error, APIError.dataError)
+            }
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 5)
     }
 }
