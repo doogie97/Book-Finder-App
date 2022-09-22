09b554865b9b59740814b721ac9e82457d19b78d
doogie97 <chris_97@naver.com>
2022-09-22 10:46:19 +0900

    💎feat : UIColor 확장을 통한 mainBackground 색상 구현 #18

---
+++ Book-Finder-App/Book-Finder-App/Utils/Extension/UIColor + Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor + Extension.swift
+//  Book-Finder-App
+//
+//  Created by 최최성균 on 2022/09/22.
+//
+
+import UIKit
+
+extension UIColor {
+    class var mainBackground: UIColor? {
+        return UIColor(named: "mainBackground")
+    }
+}
