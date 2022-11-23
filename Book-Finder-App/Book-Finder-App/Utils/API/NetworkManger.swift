aadd54a31a9a2ec5ae2b339ab04db18ce3619177
doogie97 <chris_97@naver.com>
2022-11-23 11:08:58 +0900

    💎feat : async/await를 이용하는 newRequest메서드 생성

--- Book-Finder-App/Book-Finder-App/Utils/API/NetworkManger.swift
+++ Book-Finder-App/Book-Finder-App/Utils/API/NetworkManger.swift
@@ -9,6 +9,7 @@ import Foundation
 
 protocol NetworkMangerable {
     func request(api: APIable, completion: @escaping (Result<Data, APIError>) -> Void)
+    func newRequest(api: APIable) async throws -> Data
 }
 
 struct NetworkManger: NetworkMangerable {
@@ -48,6 +49,23 @@ struct NetworkManger: NetworkMangerable {
             }
         }
         dataTask.resume()
+    }
+    
+    func newRequest(api: APIable) async throws -> Data {
+        guard let url = makeURL(api: api) else {
+            throw APIError.urlError
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = api.method.string
+        
+        let result: (data: Data, response: URLResponse) = try await URLSession.shared.data(for: request) //추후에는 customDataTask에 dataTask를 data로 변경할 필요 있음
+        
+        guard let response = result.response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
+            throw APIError.responseError
+        }
+        
+        return result.data
     }
     
     private func makeURL(api: APIable) -> URL? {
