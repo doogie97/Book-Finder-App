23625ff2cf89e1b7f4d1a3fe8a5d5995605dcae1
doogie97 <chris_97@naver.com>
2022-12-05 10:39:47 +0900

    💎feat : Alamofire를 통한 새로운 request메서드 생성

--- Book-Finder-App/Book-Finder-App/Utils/API/NetworkManger.swift
+++ Book-Finder-App/Book-Finder-App/Utils/API/NetworkManger.swift
@@ -5,10 +5,11 @@
 //  Created by 최최성균 on 2022/09/06.
 //
 
-import Foundation
+import Alamofire
 
 protocol NetworkMangerable {
     func request(api: APIable, completion: @escaping (Result<Data, APIError>) -> Void)
+    func newRequest<T: Decodable>(api:APIable, resultType: T.Type, completion: @escaping (Result<T, APIError>) -> Void)
 }
 
 struct NetworkManger: NetworkMangerable {
@@ -58,5 +59,34 @@ struct NetworkManger: NetworkMangerable {
         }
         
         return component?.url
+    }
+}
+
+
+//Alamofire
+extension NetworkManger {
+    func newRequest<T: Decodable>(api:APIable, resultType: T.Type, completion: @escaping (Result<T, APIError>) -> Void) {
+        let baseURL = api.host + api.path
+        
+        let request = AF.request(baseURL, method: api.method, parameters: api.params)
+        
+        request.responseDecodable(of: resultType) { result in
+            guard result.error == nil else {
+                completion(.failure(APIError.transportError))
+                return
+            }
+            
+            guard let response = result.response, (200...299).contains(response.statusCode) else {
+                completion(.failure(APIError.responseError))
+                return
+            }
+            
+            guard let data = result.value else {
+                completion(.failure(APIError.dataError))
+                return
+            }
+            
+            completion(.success(data))
+        }
     }
 }
