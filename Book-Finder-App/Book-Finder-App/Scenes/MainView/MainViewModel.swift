b15a50b5254650faaf321ee56e1585e06e65d095
doogie97 <chris_97@naver.com>
2022-12-05 10:43:46 +0900

    ♻️refactor : getSearchInfo의 request메서드 교체

--- Book-Finder-App/Book-Finder-App/Scenes/MainView/MainViewModel.swift
+++ Book-Finder-App/Book-Finder-App/Scenes/MainView/MainViewModel.swift
@@ -55,27 +55,22 @@ final class MainViewModel: MainViewModelable {
     private func getSearchInfo() {
         startLoading.accept(())
         let api = BookAPIModel(bookTitle: searchText, startIndex: startIndex, maxResult: maxResult, method: .get)
-        networkManager.request(api: api) { [weak self] result in
+
+        networkManager.newRequest(api: api, resultType: SearchResult.self) { [weak self] result in
             switch result {
-            case .success(let data):
-                do {
-                    let searchResult = try self?.dataDecoder.parse(data: data, resultType: SearchResult.self)
-                    
-                    guard let totalItems = searchResult?.totalItems, totalItems != 0 else {
-                        self?.showAlert.accept("검색 결과가 없습니다")
-                        self?.totalItems.accept(0)
-                        self?.stopLoading.accept(())
-                        return
-                    }
-    
-                    self?.totalItems.accept(totalItems)
-                    
-                    let oldItems = self?.items.value ?? []
-                    let newItems = oldItems + (searchResult?.items ?? [])
-                    self?.items.accept(newItems)
-                } catch let error{
-                    self?.showAlert.accept(error.errorMessage)
+            case .success(let searchResult):
+                guard let totalItems = searchResult.totalItems, totalItems != 0 else {
+                    self?.showAlert.accept("검색 결과가 없습니다")
+                    self?.totalItems.accept(0)
+                    self?.stopLoading.accept(())
+                    return
                 }
+                
+                self?.totalItems.accept(totalItems)
+                
+                let oldItems = self?.items.value ?? []
+                let newItems = oldItems + (searchResult.items ?? [])
+                self?.items.accept(newItems)
             case .failure(let error):
                 self?.showAlert.accept(error.errorMessage)
             }
